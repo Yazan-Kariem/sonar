e0a3019eb13386ca68e4f723e2067da3a945d45d
YazanM23 <112900360+YazanM23@users.noreply.github.com>
2023-08-06 16:35:02 +0300

    Update ShowLivedInFeature.java

--- src/main/loginfeature/ShowLivedInFeature.java
+++ src/main/loginfeature/ShowLivedInFeature.java
@@ -6,14 +6,14 @@ import io.cucumber.java.en.Then;
 
 import java.sql.SQLException;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class ShowLivedInFeature {
     ShowLivedIn obj=new ShowLivedIn();
     @Given("tenant pressed {string} to view peoples lived in houses")
     public void tenantPressedToViewPeoplesLivedInHouses(String arg0) {
 
-            assertTrue(arg0.equals("4"));
+        assertEquals("4", arg0);
 
     }
 
@@ -35,20 +35,20 @@ public class ShowLivedInFeature {
     @Given("tenant pressed {string} to view peoples who lived in house")
     public void tenantPressedToViewPeoplesWhoLivedInHouse(String arg0) {
 
-            assertTrue(arg0.equals("4"));
+        assertEquals("4", arg0);
 
     }
     @And("theres no peoples in houses id {string}")
     public void theresNoPeoplesInHousesId(String arg0) throws SQLException {
 
-            assertTrue(!obj.isLived(arg0));
+        assertFalse(obj.isLived(arg0));
 
     }
 
     @Then("this peoples username will not appear in house id {string}")
     public void thisPeoplesUsernameWillNotAppear(String string) throws SQLException {
 
-            assertTrue(!obj.displayLived(string));
+        assertFalse(obj.displayLived(string));
 
 
     }
